3bb9a34a10c10b1e03f42d0505ddcec5f2274cbd
ArrthurGeronimo <artha.desenvolvimento@gmail.com>
2019-09-12 20:02:22 -0300

    vai2

--- resources/js - Copia/UserApp/TesteUi.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class TesteUi extends Component {
-  render() {
-    return (
-    <h1>Teste</h1>
-    );
-  }
-}
--- resources/js - Copia/user.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-//Semantic-UI-CSS
-import 'semantic-ui-css/semantic.min.css';
-
-import UserLayout from './UserApp';
-
-ReactDOM.render(<UserLayout />, document.getElementById('UserApp'));
